b1e3479a820a15691f5590bb7cda2bd2bca1c108
DAHYUN00 <kimdahyun000@inha.edu>
2023-10-28 17:54:17 +0900

    week9

--- routes/locals.js
+++ routes/locals.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
 //렌더링: ejs 파일
+
+//즐겨찾기
+router.get('/favrite', function(req, res){
+  res.render('index', {title:'즐겨찾기', pageName:'locals/favorite.ejs'})
+});
+
 module.exports = router;
