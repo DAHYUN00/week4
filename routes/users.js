99c834bd70202c55dc22c446ad70f4399d48b1f3
DAHYUN00 <kimdahyun000@inha.edu>
2023-11-04 18:16:14 +0900

    week10

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,15 @@ module.exports = router;
 //로그인페이지 이동
 router.get('/login', function(req, res){
   res.render('index', {title:'로그인', pageName:'users/login.ejs'});
-});
+})
 
 //회원가입페이지 이동
 router.get('/join', function(req, res){
   res.render('index', {title:'회원가입', pageName:'users/join.ejs'});
-});
+})
 
+//마이페이지
+router.get('/mypage', function(req, res){
+  res.render('index', {title: '마이페이지', pageName:'users/mypage.ejs'});
+})
 module.exports = router;
