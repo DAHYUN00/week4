9543ad9b898f199fab7bfb5cc4f6e70312213d11
DAHYUN00 <kimdahyun000@inha.edu>
2023-11-25 18:08:55 +0900

    week13

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* 도서검색 page. */
+/* 도서검색 */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
 });
 
+
 //장바구니 페이지 이동
-router.get('/cart', function(req, res, next){
-  res.render('index', {title:'장바구니', pageName:'books/cart.ejs'}) //index 이거 시험
+router.get('/cart', function(req, res) {
+  res.render('index', { title: '장바구니', pageName:'books/cart.ejs' });
 });
+
 module.exports = router;
